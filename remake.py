3b69d10ce877a9558c385d92ce3fa0a7022101b1
Inug4mi <62967614+inug4mi@users.noreply.github.com>
2026-06-03 09:00:16 -0500

    Update remake.py

--- remake.py
+++ remake.py
@@ -8,18 +8,66 @@ csv_path = os.path.join(BASE_DIR, "LIBRODEREGISTRO.csv")
 
 df = pd.read_excel(excel_path)
 
-df = df[
-    [
-        "Número de Registro",
-        "Fecha de ingreso",
-        "Denominación del Objeto",
-        "Cultura",
-        "Materiales",
-        "Zona Arqueológica",
-        "País"
-    ]
+# limpiar nombres
+df.columns = df.columns.str.strip()
+#
+# columnas útiles
+columnas = [
+    "Número de Registro",
+    "Número de Inventario",
+    "Número de Tenencia",
+    "Colección",
+    "Tipo de colección",
+    "Área",
+    "Subárea",
+    "Grupo de Colección",
+    "Fecha de ingreso",
+    "Denominación del Objeto",
+    "Forma",
+    "Observaciones",
+    "Categoría",
+    "Avalúo",
+    "Cultura",
+    "Zona Arqueológica",
+    "Nombre Sitio Arqueológico",
+    "Corregimiento/inspección/vereda",
+    "Ciudad",
+    "Departamento",
+    "País",
+    "Periodo",
+    "Cronología",
+    "Materiales",
+    "Materia Prima",
+    "Unidad soporte",
+    "Color",
+    "Técnica elaboración",
+    "Técnica decoración",
+    "Técnica acabado",
+    "Unidad medida lineal",
+    "Alto",
+    "Ancho",
+    "Largo",
+    "Profundidad",
+    "Peso",
+    "Ubicación Interna",
+    "Montaje",
+    "Ubicación Externa",
+    "Fecha de revisión"
 ]
 
-df.to_csv(csv_path, index=False)
+# filtrar columnas existentes
+columnas_existentes = [
+    c for c in columnas if c in df.columns
+]
 
-print("CSV creado en:", csv_path)
+df = df[columnas_existentes]
+
+# exportar
+df.to_csv(
+    csv_path,
+    index=False
+)
+
+print("CSV creado en:")
+print(csv_path)
+print(f"Columnas exportadas: {len(columnas_existentes)}")
